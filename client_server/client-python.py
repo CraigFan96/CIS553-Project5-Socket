2c2970b3b0360f0d8414c3a6896467ac5db30603
Jimmy Xue <jameskxue@gmail.com>
2019-04-08 22:23:53 -0400

    some changes

--- client_server/client-python.py
+++ client_server/client-python.py
@@ -3,9 +3,44 @@ import socket
 
 SEND_BUFFER_SIZE = 2048
 
+'''
+	Each client program should contact a server, 
+	read a message from stdin, send the message, and exit.
+	Each client should read and send the message exactly as 
+	it appears in stdin until reaching an EOF (end-of-file).
+	Each client should take two command-line arguments: the 
+	IP address of the server and the port number of the server.
+	Each client must be able to handle arbitrarily large messages 
+	by iteratively reading and sending chunks of the message, 
+	rather than reading the whole message into memory first.
+	Each client should handle partial sends (when a socket only 
+	transmits part of the data given in the last send call) by attempting 
+	to re-send the rest of the data until it has all been sent.
+	Each client should gracefully handle error values potentially 
+	returned by socket programming library functions.
+'''
+
+
 def client(server_ip, server_port):
     """TODO: Open socket and send message from sys.stdin"""
-    pass
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, SEND_BUFFER_SIZE)
+    s.connect((server_ip, server_port))
+    data = s.recv(SEND_BUFFER_SIZE)
+    print(sys.stdin)
+    s.sendall('HELLO WORLD')
+    for line in sys.stdin:
+
+        total = len(line)
+        begin = 0
+        end = min(total, SEND_BUFFER_SIZE)
+        while total > 0:
+            print(line[begin:end])
+            total -= end
+            begin = end
+            end = min(end + total, SEND_BUFFER_SIZE)
+            s.send(line[begin:end])
+    s.close()
 
 
 def main():
@@ -14,7 +49,9 @@ def main():
         sys.exit("Usage: python client-python.py [Server IP] [Server Port] < [message]")
     server_ip = sys.argv[1]
     server_port = int(sys.argv[2])
+    print(sys.stdin)
     client(server_ip, server_port)
+
 
 if __name__ == "__main__":
     main()
--- client_server/server-python.py
+++ client_server/server-python.py
@@ -3,10 +3,35 @@ import socket
 
 RECV_BUFFER_SIZE = 2048
 QUEUE_LENGTH = 10
+HOST = '127.0.0.1'
+'''
+Each server program should listen on a socket, wait for a client to connect,
+receive a message from the client, print the message to stdout,
+and then wait for the next client indefinitely.
+Each server should take one command-line argument: the port number to
+listen on for client connections.
+Each server should accept and process client communications in an infinite
+loop, allowing multiple clients to send messages to the same server.
+The server should only exit in response to an external signal (e.g. SIGINT from pressing ctrl-c).
+Each server should maintain a short (10) client queue and handle multiple client
+connection attempts sequentially. In real applications, a TCP server would fork a new process to handle each client connection concurrently, but that is not necessary for this assignment.
+Each server should gracefully handle error values potentially returned by socket
+programming library functions (see specifics for each language below).
+'''
+
 
 def server(server_port):
     """TODO: Listen on socket and print received message to sys.stdout"""
-    pass
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.bind((HOST, server_port))
+    s.listen(QUEUE_LENGTH)
+    conn, addr = s.accept()
+    print("Initiating server")
+    while True:
+        data = conn.recv(RECV_BUFFER_SIZE)
+        print(data)
+    conn.close()
 
 
 def main():
@@ -15,6 +40,7 @@ def main():
         sys.exit("Usage: python server-python.py [Server Port]")
     server_port = int(sys.argv[1])
     server(server_port)
+
 
 if __name__ == "__main__":
     main()
